8467e0ddccca88fd87fe28a729b733b434a3d31c
anubhav-sri80 <anu.ritz80@gmail.com>
2020-04-05 18:22:40 +0530

    Added Profile picture functionality

--- users/views.py
+++ users/views.py
@@ -4,11 +4,11 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 # Create your views here.
-def register(request):
+def register(request): 
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account Created Successfully! You can Login now.')
             return redirect('login') 
