839fe9a5b52dd7e80642777ef211341e564dc940
anubhav-sri80 <anu.ritz80@gmail.com>
2020-04-06 16:12:02 +0530

    Added Create,Update,Delete Post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView)
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), #learn
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), #learn
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), #learn
     path('about/',views.about, name='blog-about'),
 ]
